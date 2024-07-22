0af795892a9ab039c0fbceaaf4423c8a26f8a50d
andersongilvan <torresdinho8@gmail.com>
2024-07-22 18:27:55 -0300

    iniciando projet spring

---
+++ src/main/java/br/com/screenmatch/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,30 @@
+package br.com.screenmatch.screenmatch;
+
+import br.com.screenmatch.model.DadosSerie;
+import br.com.screenmatch.service.ConsumoApi;
+import br.com.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		var consumoApi = new ConsumoApi();
+		String url = "http://www.omdbapi.com/?t=gilmore+girls&apikey=69246a71";
+		var json = consumoApi.obterDados(url);
+
+
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+
+	}
+}
